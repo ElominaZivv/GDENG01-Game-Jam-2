bd3a5e39cb4a37fd811da66df3c4b874de8ab38f
pekep <168715052+ElominaZivv@users.noreply.github.com>
2025-07-20 20:45:00 +0800

    All Tarot Prefabs Implemented
    - All Major Arcana prefab variants 0 -> 21 implemented
    - Selecting animation
    - Flip animation

---
+++ GDENG01 Game Jam 2/Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Used for detecting which gameobject the player clicked
+
+        // Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        // RaycastHit hit;
+        // if (Physics.Raycast(ray, out hit))
+        // {
+        //     // Debug.Log(hit.collider.name);
+        // }      
+    }
+}
--- GDENG01 Game Jam 2/Assets/Scripts/TarotCardController.cs
+++ GDENG01 Game Jam 2/Assets/Scripts/TarotCardController.cs
@@ -2,20 +2,40 @@ using UnityEngine;
 
 public class TarotCardController : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] int id;
     [SerializeField] string name;
     [SerializeField] string upright_description;
     [SerializeField] string reversed_description;
     [SerializeField] bool upright = true;
+    [SerializeField] public bool isSelected = false;
+    [SerializeField] bool isFlipped = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("isSelected", isSelected);
+        animator.SetBool("isFlipped", isFlipped);
+    }
+
+    void OnMouseOver()
+    {
+        isSelected = true;
+    }
+
+    void OnMouseExit()
+    {
+        isSelected = false;
+    }
+
+    void OnMouseDown()
+    {
+        isFlipped = !isFlipped;
     }
 }

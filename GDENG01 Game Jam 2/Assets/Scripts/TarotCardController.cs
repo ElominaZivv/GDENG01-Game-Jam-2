49c7c83ad51b7990f9bbcbe2c213adb6143e3bd8
pekep <168715052+ElominaZivv@users.noreply.github.com>
2025-07-20 17:49:14 +0800

    Tarot Card WIP

---
+++ GDENG01 Game Jam 2/Assets/Scripts/TarotCardController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TarotCardController : MonoBehaviour
+{
+    [SerializeField] int id;
+    [SerializeField] string name;
+    [SerializeField] string upright_description;
+    [SerializeField] string reversed_description;
+    [SerializeField] bool upright = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
